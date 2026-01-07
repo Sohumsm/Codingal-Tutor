4773f638612a34d3448df9c676249db263cd8a83
Sohumsm <Sohummudgal@hotmail.com>
2026-01-07 17:31:04 +0000

    for.loops

--- nested conditional statement/activity3.py
+++ nested conditional statement/activity3.py
@@ -1,2 +1,18 @@
 print("Select your ride: ")
 print("1. Bike")
+print("2. Car")
+
+choice =int(input("Enter your choices: "))
+
+if(choice == 1):
+    print("What type of bike?")
+    print("1.Scooty\n")
+    print("Scooter\n")
+
+    choice2 = int(input("Enter your choice 2: "))
+    if choice2 == 1:
+        print("You have selected scooty")
+    else:
+        print("You have selected scooter")
+
+    
