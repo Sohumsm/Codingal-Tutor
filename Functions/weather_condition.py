096451a54efda615ff32f6652d8b485ed47e4433
Sohumsm <Sohummudgal@hotmail.com>
2026-01-28 17:21:15 +0000

    weather_condition

---
+++ Functions/weather_condition.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("The weather is pleasant in:",spring)
+    print("The weather is same in:",autumn) 
+
+spring = "autmun123"
+autumn = spring
+weather_condition()  
