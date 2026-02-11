d5347fcd8c49b85895705aeb6a3246f452936009
Sohumsm <Sohummudgal@hotmail.com>
2026-02-11 17:21:42 +0000

    Multiple_exception

---
+++ Exceptions/Multiple_exception.py
@@ -0,0 +1,17 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 / num2
+    print("Result is : ", result)
+
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+except ValueError:
+    print("Pleae enter numerical value")
+except NameError as ex:
+    print("The exception is ", ex)
+except:
+    print("Some error occurred")
+
+finally:
+    print("I will execute no matter what happens")
