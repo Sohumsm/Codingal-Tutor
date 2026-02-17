43d26bc59da9786775be4968f51f3b55a22dd97c
Sohumsm <Sohummudgal@hotmail.com>
2026-02-17 17:42:07 +0000

    mathematicaloperators.py

---
+++ Random Module/Mathematicaloperators.py
@@ -0,0 +1,11 @@
+import math
+
+print('the Floor and Celling value  of  23.56 are : ' + str(math.floor(23.56)))
+
+x = 10
+y = -15
+
+print('Ther value of x after copying the sign from y is: ' + str(math.copysign(x, y)))
+
+print('Absolute value of -96 and 56 are: ' + str(math.fabs(-96)) + ','+ str(math.fabs(56)))
+print('The GCD of 24 and 56 : ' + str(math.gcd(24, 56)))
