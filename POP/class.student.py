3175dd61ab2671daae3dc00b557f279b6763e9f4
Sohumsm <Sohummudgal@hotmail.com>
2026-03-24 17:34:07 +0000

    parrot

--- POP/class.student.py
+++ POP/class.student.py
@@ -2,8 +2,4 @@ class student:
     grade = 10
     print("Hi I am a student of grade", grade)
 
-ob2 = student()class student:
-    grade = 10
-    print("Hi I am a student of grade", grade)
-
 ob2 = student()
