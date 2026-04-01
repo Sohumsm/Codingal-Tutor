92b59cd09ac446ac4f85caf744db19b775e70bd9
Sohumsm <Sohummudgal@hotmail.com>
2026-04-01 18:31:37 +0100

    Super penguin

---
+++ Inheritance-OOPS/Superpenguin.py
@@ -0,0 +1,27 @@
+class Bird:
+
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+
+class Penguin(Bird):
+
+    def __init__(self):
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        super().__init__()
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
