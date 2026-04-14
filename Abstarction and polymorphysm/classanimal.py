7609fcd5af2febfa099685fa0458f417efbc35b7
Sohumsm <Sohummudgal@hotmail.com>
2026-04-14 18:25:52 +0100

    class animal

---
+++ Abstarction and polymorphysm/classanimal.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def move(self):
+        pass
+
+class Human(Animal):
+    def move(self):
+        print("I can walk and run")
+
+class Snake(Animal):
+    def move(self):
+        print("I can crawl")
+
+R = Human()
+R.move()
+
+K = Snake()
+K.move()
